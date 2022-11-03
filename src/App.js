9aaaa271ac4db7d492a414a7ef763bf299327b31
nandysoham <nandysoham16@gmail.com>
2022-11-03 23:53:39 +0530

    initial commmit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { } from "react";
+import "./App.css";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+// import Stack from '@mui/material/Stack';
+import Button from "@mui/material/Button";
+import Navbar from "./Navbar";
+import { useForm } from "react-hook-form";
+import {saveAs} from "file-saver";
 function App() {
+  
+  const { register, handleSubmit } = useForm();
+  const submitdata = async (data) => {
+    const response = await fetch(
+      `https://sohamgitscrapper.herokuapp.com//${data.username}/${data.repo}`,
+      {
+        method: "GET",
+      }
+    );
+
+    const resp = await response.json();
+    console.log(resp);
+    
+    const file = new Blob(
+      [JSON.stringify(resp)],
+      {type: 'application/json'});
+      const name = 'Report.json';
+      saveAs(file, name);
+  };
+
+
+  const onSubmit = (data) => {
+    console.log(data);
+    submitdata(data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div
+        style={{
+          minHeight: "100vh",
+          minWidth: "100vw",
+          backgroundImage: "linear-gradient(#141e30 , #243b55)",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Box
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 1, width: "25ch" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              id="outlined-basic"
+              label="GitHub username"
+              variant="outlined"
+              InputLabelProps={{
+                className: "textfield_label",
+              }}
+              InputProps={{
+                className: "textfield_label",
+              }}
+              {...register("username")}
+            />
+            <TextField
+              id="outlined-basic"
+              label="Repository Name"
+              variant="outlined"
+              InputLabelProps={{
+                className: "textfield_label",
+              }}
+              InputProps={{
+                className: "textfield_label",
+              }}
+              {...register("repo")}
+            />
+          </Box>
+          {/* <Stack direction="row" spacing={2}> */}
+          <Button
+            className="my-4"
+            variant="contained"
+            color="success"
+            type="submit"
+          >
+            Find Commits
+          </Button>
+          {/* </Stack> */}
+        </form>
+        
+        
+      </div>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="/">  Soham's Commit Extractor</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  
+</nav>
+        </div>
+    )
+}
+
+export default Navbar
